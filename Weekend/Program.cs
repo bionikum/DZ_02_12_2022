2f7d80d5318ac2c8b1834f5ad2f5933ce402a8c4
Ilya <ilya@bobrikov.ru>
2022-12-02 22:34:20 +0500

    3 задача

---
+++ Weekend/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число, соответствующее дню недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("Ура, выходной..!!!");
+}
+else if (n < 1 || n > 7)
+{
+    Console.WriteLine("День недели с таким номером не существует..");
+}
+
+else
+{
+    Console.WriteLine("Не является выходным...((");
+}
