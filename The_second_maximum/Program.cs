055cde54ac5c1058c63990b919182f0ba0472b7b
Ilya <ilya@bobrikov.ru>
2022-12-03 00:07:52 +0500

    Второе максимальное значение до 0 в числе

---
+++ The_second_maximum/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = 1;
+if (n > 99)
+{
+    while (n > 999 && !(n1 == 0))
+    {
+        n = n / 10;
+        n1 = n % 10;
+    }
+}
+    n = n / 100;
+    n = n % 10;
+
+    Console.WriteLine($"Значение второго максимума:  {n}");
+
+
