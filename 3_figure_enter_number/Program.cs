afb01ebf9a9db2e38c7f2b2d36c119a148ea1f17
Ilya <ilya@bobrikov.ru>
2022-12-02 22:12:32 +0500

    Нахождение 3 цифры у числа

---
+++ 3_figure_enter_number/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (n < 100)
+{
+    Console.WriteLine("Вы ввели не подходящее число");
+
+}
+else if (n > 999)
+{
+    while (n > 999)
+    {
+        n = n / 10;
+    }
+
+    int n1 = n % 10;
+    Console.WriteLine($"Третья цифра введенного числа: {n1}");
+}
