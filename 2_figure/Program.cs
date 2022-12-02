5543abaf9ce23e7211b88c2a916babb7928eeedd
Ilya <ilya@bobrikov.ru>
2022-12-02 16:17:32 +0500

    1 задача

---
+++ 2_figure/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (n < 100 || n > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int n1 = n % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine($"Вторая цифра введенного числа: {n2}");
+}
+
+
