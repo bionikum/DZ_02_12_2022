1eeea601c4be63a4729176bbbee1796c5c7bdc49
Ilya <ilya@bobrikov.ru>
2022-12-03 15:52:21 +0500

    Нахождение второго макисмального из введенных чисел

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,18 +1,19 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите числа: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int i = 0;
-int x = 0;
-while (n != 0 && i < 100)
+int max1 = 0;
+int max2 = 0;
+while (n != 0)
 {
-    n = Convert.ToInt32(Console.ReadLine());
-    i = i + n;
-    while (i < i + n)
+    if (n > max1)
     {
-        n = Convert.ToInt32(Console.ReadLine());
-       
+        max2 = max1;
+        max1 = n;
     }
-
+    else if (n > max2)
+    {
+        max2 = n;
+    }
+    n = Convert.ToInt32(Console.ReadLine());
 }
-
-Console.WriteLine($"Значение второго максимума: {i}");
+Console.WriteLine($"Значение второго максимального: {max2}");
